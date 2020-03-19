b33dc5a062fad8fe9f1ceb09f85abf8eee10279b
Jake Kile <jkile9@gmail.com>
2020-03-19 10:18:02 -0700

    implement changes made by Trevor

--- routes/stateCovid-api-routes.js
+++ routes/stateCovid-api-routes.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
 
   // GET route for getting all of the covid
   app.get("/api/statecovid", function(req, res) {
-    db.statecovid.findAll({})
+      statecovid.findAll({})
       .then(function(dbstateCovid) {
         res.json(dbstateCovid);
       });
@@ -16,7 +16,7 @@ module.exports = function(app) {
 
   // Get route for returning statecovids of a specific state
   app.get("/api/statecovids/state/:state", function(req, res) {
-    db.stateCovid.findAll({
+    stateCovid.findAll({
       where: {
         state: req.params.state
       }
@@ -28,7 +28,7 @@ module.exports = function(app) {
 
   // Get route for retrieving a single stateCovid date
   app.get("/api/statecovids/:date", function(req, res) {
-    db.stateCovid.findOne({
+      stateCovid.findOne({
       where: {
         date: req.params.date
       }
