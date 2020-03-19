e099c4efc9385fe419f04bf91958516732e51083
Trevor Dorn <dorntrevor7@gmail.com>
2020-03-19 13:47:21 -0700

    merge with justice and jake

--- db/schema.sql
+++ db/schema.sql
@@ -2,30 +2,3 @@
 DROP DATABASE IF EXISTS covid19;
 
 CREATE DATABASE covid19;
-<<<<<<< HEAD
-=======
-USE covid19;
-
-CREATE TABLE selfReporting (
-    dateOf DATE,
-    state CHAR(2),
-    achespains BOOLEAN NOT NULL default false,
-    runnynose BOOLEAN NOT NULL default false,
-    sorethroat BOOLEAN NOT NULL default false,
-    cough BOOLEAN NOT NULL default  false,
-    fever BOOLEAN NOT NULL default false,
-    difficultyBreathing BOOLEAN NOT NULL default false,
-    othersymptoms VARCHAR(140) 
-);
-
-CREATE TABLE statecovids (
-    date DATE,
-    state CHAR(2),
-    positive DECIMAL(10,4) NOT null default 0,
-    negative DECIMAL(10,4) NOT null default 0,
-    pending DECIMAL(10,4) NOT null default 0,
-    death DECIMAL(10,4) NOT null default 0,
-    total DECIMAL(10,4) NOT null default 0
-);
-
->>>>>>> 1966a62d7889207cde332c8c4a1dcf25ccdc7e86
--- models/stateCovid.js
+++ models/stateCovid.js
@@ -1,55 +1,7 @@
-<<<<<<< HEAD
+
 const DataTypes = require("sequelize");
-var sequelize = require("../config/connection");
+const sequelize = require("../config/connection");
 
-var Statecovid = sequelize.define("statecovid", {
-  dateOf: {
-    type: DataTypes.DATE
-  },
-  state: {
-    primaryKey: true,
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [2]
-    }
-  },
-  positive: {
-    type: DataTypes.DECIMAL(10,2),
-    allowNull: false,
-    defaultValue: 0.0
-  },
-  negative: {
-    type: DataTypes.DECIMAL(10,2),
-    allowNull: false,
-    defaultValue: 0.0
-  },
-  pending: {
-    type: DataTypes.DECIMAL(10,2),
-    allowNull: false,
-    defaultValue: 0.0
-  },
-  death: {
-    type: DataTypes.DECIMAL(10,2),
-    allowNull: false,
-    defaultValue: 0.0
-  },
-  total: {
-    type: DataTypes.DECIMAL(10,2),
-    allowNull: false,
-    defaultValue: 0.0
-  }
-
-});
-
-Statecovid.sync();
-
-module.exports = Statecovid;
-=======
-const sequelize = require("sequelize");
-// const Papa = require("papaparse");
-// const fs = require("fs");
-// const file = require("../db/seed.csv");
 
 module.exports = function(sequelize, DataTypes) {
   var statecovid = sequelize.define("statecovid", {
@@ -92,4 +44,3 @@ module.exports = function(sequelize, DataTypes) {
 
   return statecovid;
 };
->>>>>>> 1966a62d7889207cde332c8c4a1dcf25ccdc7e86
