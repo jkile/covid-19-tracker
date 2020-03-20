f4e954598abc639a4067c00c029741f8100f7186
Jake Kile <jkile9@gmail.com>
2020-03-20 13:40:11 -0700

    setup db sync and removed connection file

--- server.js
+++ server.js
@@ -25,10 +25,8 @@ require("./routes/html-routes")(app);
 
 // // Syncing our sequelize models and then starting our Express app
 // // =============================================================
-// db.sequelize.sync().then(function() {
-//   app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
-// });
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT)});
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
