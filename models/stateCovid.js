ac76b10e48bd535fd485567b7ac1fa8cb406b91f
Jake Kile <jkile9@gmail.com>
2020-03-23 14:41:14 -0700

    removed primary key from state in models

--- models/stateCovid.js
+++ models/stateCovid.js
@@ -5,7 +5,6 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.DATEONLY
     },
     state: {
-      primaryKey: true,
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
